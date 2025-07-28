f7daea29c6b673d7d45262e751885837fa2e28aa
sudeeeeeeeeeep <sudeeponline2024@gmail.com>
2025-07-28 08:05:31 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// This file is a placeholder for now.
+// In our next step, we will create a Firebase project and paste the configuration object here.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyADC_AJcDYzbrJ89FinBlBcCPFX9xbQSvg",
+  authDomain: "townboat-4f89d.firebaseapp.com",
+  projectId: "townboat-4f89d",
+  storageBucket: "townboat-4f89d.firebasestorage.app",
+  messagingSenderId: "637400300941",
+  appId: "1:637400300941:web:867ee7106a221fb17651c4",
+  measurementId: "G-MK6M762TJ2"
+};
